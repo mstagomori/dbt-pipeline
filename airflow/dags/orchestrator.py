1af0a1682ca1860bb1e2041bd1e8b5b0300c4b7b
Matheus Schmidt Tagomori <mathschmidtt@gmail.com>
2026-02-06 14:47:05 -0300

    Add dbt DAG to Airflow

--- airflow/dags/orchestrator.py
+++ airflow/dags/orchestrator.py
@@ -2,6 +2,8 @@ import sys
 from airflow import DAG
 from airflow.providers.standard.operators.python import PythonOperator
 from datetime import datetime, timedelta
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount
 
 sys.path.append('/opt/airflow/api-request')
 def safe_main_callable():
@@ -15,7 +17,7 @@ default_args = {
 }
 
 dag = DAG(
-    dag_id='financial-data-orchestrator',
+    dag_id='financial-api-dbt-orchestrator',
     default_args=default_args,
     schedule=timedelta(minutes=1)
 )
@@ -25,3 +27,23 @@ with dag:
         task_id='ingest_data',
         python_callable=safe_main_callable
     )
+
+    task2 = DockerOperator(
+        task_id='transform_data',
+        image='ghcr.io/dbt-labs/dbt-postgres:1.9.latest',
+        command='run',
+        working_dir= '/usr/app',
+        mounts=[
+            Mount(source='/mnt/d/Arquivos/Trabalho/Projetos/dbt-pipeline/dbt/my_project', 
+                  target='/usr/app', 
+                  type='bind'),
+            Mount(source='/mnt/d/Arquivos/Trabalho/Projetos/dbt-pipeline/dbt/profiles.yml', 
+                  target='/root/.dbt/profiles.yml', 
+                  type='bind')
+        ],
+        network_mode='dbt-pipeline_my-network',
+        docker_url='unix://var/run/docker.sock',
+        auto_remove='success'
+    )
+
+    task1 >> task2
